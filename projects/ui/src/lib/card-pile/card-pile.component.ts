ef3c315ae179962c8cf7a2d0fb28e65d15ccf31e
Kalyan <kalyanasundaramannam@nbnco.com.au>
2019-12-10 18:27:49 +1030

    add media query

--- projects/ui/src/lib/card-pile/card-pile.component.ts
+++ projects/ui/src/lib/card-pile/card-pile.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'ui-card-pile',
-  templateUrl: './card-pile.component.html'
+  templateUrl: './card-pile.component.html',
+  styleUrls: ['./card-pile.component.scss']
 })
 export class CardPileComponent implements OnInit {
   @Input() cards: Card[];
--- projects/ui/src/lib/card/card.component.ts
+++ projects/ui/src/lib/card/card.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'ui-card',
-  templateUrl: './card.component.html'
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
   @Input() card: Card;
