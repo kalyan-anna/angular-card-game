a524d901fff9ef9c210d44309b7f1185e46e3c74
Kalyan <kalyanasundaramannam@nbnco.com.au>
2019-12-10 18:13:46 +1030

    fix alignment

--- src/app/containers/game/game.component.ts
+++ src/app/containers/game/game.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'snap-game',
-  templateUrl: './game.component.html',
-  styleUrls: ['./game.component.scss']
+  templateUrl: './game.component.html'
 })
 export class GameComponent implements OnInit {
   cards: Card[];
@@ -14,7 +13,19 @@ export class GameComponent implements OnInit {
     this.cards = [
       { id: 'a1', shape: 'a', number: 1 },
       { id: 'a2', shape: 'a', number: 2 },
-      { id: 'a3', shape: 'a', number: 3 }
+      { id: 'a3', shape: 'a', number: 3 },
+      { id: 'a4', shape: 'a', number: 3 },
+      { id: 'a5', shape: 'a', number: 3 },
+      { id: 'a6', shape: 'a', number: 3 },
+      { id: 'a7', shape: 'a', number: 3 },
+      { id: 'a8', shape: 'a', number: 3 },
+      { id: 'a9', shape: 'a', number: 3 },
+      { id: 'a10', shape: 'a', number: 3 },
+      { id: 'a11', shape: 'a', number: 3 },
+      { id: 'a12', shape: 'a', number: 3 },
+      { id: 'a13', shape: 'a', number: 3 },
+      { id: 'a14', shape: 'a', number: 3 },
+      { id: 'a15', shape: 'a', number: 3 }
     ];
   }
 
