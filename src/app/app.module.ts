e1711627b9758253e351cdb034f8fa04632da14a
Kalyan <kalyanasundaramannam@nbnco.com.au>
2019-12-10 18:38:44 +1030

    add material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
 import { UiModule } from 'projects/ui/src/public-api';
 import { GameComponent } from './containers/game/game.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { GameComponent } from './containers/game/game.component';
   imports: [
     BrowserModule,
     UiModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
