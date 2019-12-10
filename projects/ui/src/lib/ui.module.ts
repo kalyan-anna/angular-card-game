777a2301f653768d0a28d7ad8e766d177c39ea74
Kalyan <kalyanasundaramannam@nbnco.com.au>
2019-12-10 16:08:05 +1030

    add card-pile

--- projects/ui/src/lib/ui.module.ts
+++ projects/ui/src/lib/ui.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { CardComponent } from './card/card.component';
+import { CardPileComponent } from './card-pile/card-pile.component';
 
 
 @NgModule({
-  declarations: [HeaderComponent, CardComponent],
+  declarations: [HeaderComponent, CardComponent, CardPileComponent],
   imports: [
   ],
-  exports: [HeaderComponent, CardComponent]
+  exports: [HeaderComponent, CardComponent, CardPileComponent]
 })
 export class UiModule { }
