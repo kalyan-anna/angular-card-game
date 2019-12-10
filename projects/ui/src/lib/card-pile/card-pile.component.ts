8e0f886bc450886d58646d6390f43dfd26e7e512
Kalyan <kalyanasundaramannam@nbnco.com.au>
2019-12-11 04:18:54 +1030

    top card position

--- projects/ui/src/lib/card-pile/card-pile.component.ts
+++ projects/ui/src/lib/card-pile/card-pile.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'ui-card-pile',
   templateUrl: './card-pile.component.html',
-  styleUrls: ['./card-pile.component.scss']
+  styleUrls: ['./card-pile.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CardPileComponent implements OnInit {
   @Input() cards: Card[];
@@ -15,5 +16,9 @@ export class CardPileComponent implements OnInit {
 
   identify(i: number, card: Card) {
     return card.id;
+  }
+
+  isTopCard(isLast: boolean, card: Card) {
+    return isLast && this.cards.length > 1 && card.status === 'faceup';
   }
 }
